c2942ffbfcebdb0d478416f17eeafb0a6058d0f1
tatsuya <tatsuya4649developer@gmail.com>
2021-07-10 11:26:34 +0900

    mod add

---
+++ nodes/mod.py
@@ -0,0 +1,21 @@
+import binascii
+import test
+
+def mod(nodes,data):
+	crc32 = binascii.crc32(data.encode('utf-8'))
+	index = crc32 % len(nodes)
+	return nodes[index]
+
+if __name__ == "__main__":
+	test = test.TestNode(node_count=10)
+	data = "124"
+	print("========= mod ===========")
+	test.now_nodes()
+	test.now_datas()
+	test.test(mod,test.nodes,test.datas)
+	print("========= add ===========")
+	test.append()
+	test.test(mod,test.nodes,test.datas)
+	print("========= delete ===========")
+	test.delete()
+	test.test(mod,test.nodes,test.datas)
---
+++ nodes/test.py
@@ -0,0 +1,63 @@
+import random
+import string
+
+
+class TestNode:
+	def __init__(self,node_count=10,data_count=100,str_len=10):
+		self._node_count = node_count
+		self._data_count = data_count
+		self._nodes = list()
+		self._datas = list()
+		for _ in range(self._node_count):
+			self._nodes.append(self.random_ip())
+		for _ in range(self._data_count):
+			random_str = ""
+			for _ in range(str_len):
+				random_str += random.choice(string.ascii_letters + string.digits)
+			self._datas.append(random_str)
+
+	@classmethod	
+	def random_ip(self):
+		ip = ""
+		for i in range(4):
+			dot = "." if i != 3 else ""
+			ip += f"{random.randint(0,255)}{dot}"
+		return ip
+
+	@property
+	def nodes(self):
+		return self._nodes
+	@property
+	def datas(self):
+		return self._datas
+
+	def now_nodes(self):
+		print(f"Nodes => {self.nodes}")
+	def now_datas(self):
+		print(f"Datas => {self.datas}")
+
+	# insert new IP Address
+	def append(self):
+		self._nodes.append(self.random_ip())
+	
+	# delete IP Address with Index
+	def delete(self,index=None):
+		if index is None:
+			index = random.randint(0,len(self._nodes)-1)
+		self._nodes.pop(index)
+
+	# function (nodes,data)
+	def test(self,func,nodes=None,datas=None):
+		nodes_datas = dict()
+		for node in nodes:
+			nodes_datas[node] = list()
+		if nodes is None:
+			nodes = self.nodes
+		if datas is None:
+			datas = self.datas
+		for data in datas:
+			res_node = func(nodes,data)
+			nodes_datas[res_node].append(data)
+		for key in nodes_datas.keys():
+			print(f"{key}: {nodes_datas[key]}")
+		
