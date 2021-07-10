1e6eef30056b306d9258075e0180380f8f27bf4d
tatsuya <tatsuya4649developer@gmail.com>
2021-07-10 17:08:02 +0900

    sharding mysql Dockerfile update

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t shmysql .
-docker network create shmysql
--- node1/run.sh
+++ node1/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name node1 --env-file ../ENV -v $PWD/data:/var/lib/mysql -p 13306:3306 -d shmysql
+docker run --name node1 --env-file ../ENV -p 13306:3306 -d shmysql
